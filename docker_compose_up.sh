860694894fa3e81d844f06a52c6e5aa062057779
n0nuser <pjgr2000@gmail.com>
2022-05-17 16:53:05 +0200

    Fixed create superuser in docker compose script

--- docker_compose_up.sh
+++ docker_compose_up.sh
@@ -2,10 +2,10 @@
 docker-compose up -d --build
 docker exec django-monitor-uvicorn python3 manage.py collectstatic --no-input
 # docker exec django-monitor-uvicorn python3 manage.py compress
-docker exec django-monitor-uvicorn python3 manage.py createcachetable
+# docker exec django-monitor-uvicorn python3 manage.py createcachetable
 docker exec django-monitor-uvicorn python3 manage.py migrate --noinput
 
 USER=admin
 PASS=admin
 EMAIL=admin@monitor.tfg
-echo "from web.models import CustomUser; User.objects.create_superuser('$USER', '$EMAIL', '$PASS')" | python manage.py shell
+echo "from web.models import CustomUser; User.objects.create_superuser('$USER', '$EMAIL', '$PASS')" | docker exec django-monitor-uvicorn python3 manage.py shell
