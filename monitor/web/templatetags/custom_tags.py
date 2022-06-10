2235132cfa9a3f1a72e6e4ab3f56a2b348442d2c
n0nuser <pjgr2000@gmail.com>
2022-06-10 10:23:47 +0200

    Changed Metric UI via recursive django template

---
+++ monitor/web/templatetags/custom_tags.py
@@ -0,0 +1,8 @@
+from django import template
+register = template.Library()
+
+
+@register.filter()
+def field_name_to_label(value):
+    value = value.replace('_', ' ')
+    return value.capitalize()
